63be9d5afd5ccfec49a66c32f58dfbf15ad524c3
William Lockhart <willlockhart65@gmail.com>
2024-08-20 17:46:49 -0600

    added basic snapping

--- back/Entity.cpp
+++ back/Entity.cpp
@@ -1,32 +1,27 @@
 #include "Entity.hpp"
 
 // creates playerText
-void Entity::addEntity(SDL_Renderer *ren, const char *fileLocation, SDL_Rect rect, bool turns)
+void Entity::addEntity(SDL_Renderer *ren, SDL_Rect rect)
 {
     EntityInfo E;
     SDL_Surface *tempSurface;
-    if (turns)
+
+    if (turn == X)
     {
-        if (turn == X)
-        {
-            tempSurface = IMG_Load("assets/X.png");
-            turn = O;
-        }
-        else
-        {
-            tempSurface = IMG_Load("assets/O.png");
-            rect.x += 340;
-            turn = X;
-        }
-        if (!tempSurface)
-        {
-            std::cout << "failure" << std::endl;
-        }
+        tempSurface = IMG_Load("assets/X.png");
+        turn = O;
     }
     else
     {
-        tempSurface = IMG_Load(fileLocation);
+        tempSurface = IMG_Load("assets/O.png");
+        rect.x += 340;
+        turn = X;
     }
+    if (!tempSurface)
+    {
+        std::cout << "failure" << std::endl;
+    }
+
     SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, tempSurface);
     E.entityTexture = tex;
     E.entityRect = rect;
@@ -82,7 +77,6 @@ void Entity::setPlayerRect(SDL_Rect rect, int ID)
     if (EntityList.size() > ID && ID >= 0)
     {
         EntityList.at(ID).entityRect = rect;
-        playAudio("assets/placePiece.mp3");
     }
 }
 
@@ -95,5 +89,42 @@ void Entity::playAudio(const char *fileLocation)
         {
             std::cerr << "Error playing music: " << Mix_GetError() << std::endl;
         }
+    }
+}
+
+void Entity::SnapToGrid(int positionX, int positionY, SDL_Rect tile, int &x, int &y, turnType board[3][3], SDL_Renderer *ren)
+{
+
+    int gridX = positionX / tile.w;
+    int gridY = positionY / tile.h;
+    int newPositionX = gridX * tile.w;
+    int newPositionY = gridY * tile.h;
+
+    // if the index is empty
+    if (board[gridX][gridY] == empty)
+    {
+        playAudio("assets/placePiece.mp3");
+        x = newPositionX;
+        y = newPositionY;
+        board[gridX][gridY] = turn;
+        SDL_Rect tempRect = {700, 8, 240, 240};
+        addEntity(ren, tempRect);
+    }
+    else
+    {
+        x = 870;
+        y = 8;
+    }
+}
+
+void Entity::setTurn(int t)
+{
+    if (t)
+    {
+        turn = X;
+    }
+    else
+    {
+        turn = O;
     }
 }
